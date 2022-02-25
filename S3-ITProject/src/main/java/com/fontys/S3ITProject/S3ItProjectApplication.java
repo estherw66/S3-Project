578b2385cd61cd66a58b315a51a81addb21ab1b4
Esther Wolfs <estherwolfs@Esthers-MacBook-Pro.local>
2022-02-25 11:31:30 +0100

    add project

---
+++ S3-ITProject/src/main/java/com/fontys/S3ITProject/S3ItProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fontys.S3ITProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ItProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ItProjectApplication.class, args);
+	}
+
+}
---
+++ S3-ITProject/src/test/java/com/fontys/S3ITProject/S3ItProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontys.S3ITProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ItProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
